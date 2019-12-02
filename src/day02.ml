824bc5a6e1465b47c176d4ad822ed5c409fb5d37
Robin Larsson <gwaerondor@gmail.com>
2019-12-02 15:35:03 +0800

    Add solution to day02 part 2.
    Also refactor lots of stuff.

--- src/day02.ml
+++ src/day02.ml
@@ -1,49 +1,58 @@
 open Core
 open Aoc_lib
 type op = Add | Mul | Break
-
-let input =
-  Aoc_lib.csv_of_file "input02.txt"
-  |> List.map ~f:int_of_string
-  |> List.to_array
+exception Bad_argument
 
 let get_operator params pos =
   match params.(pos) with
-  | 1 -> Some Add
-  | 2 -> Some Mul
-  | 99 -> Some Break
-  | _ -> None
+  | 1 -> Add
+  | 2 -> Mul
+  | 99 -> Break
+  | _ -> raise Bad_argument
 
 let op_to_fun = function
   | Add -> ( + )
   | Mul -> ( * )
-  | Break -> (fun _ _ -> -1)
+  | Break -> raise Bad_argument
 
 let rec execute ~params ~pos =
   let operator = get_operator params pos in
   match operator with
-  | Some Break -> params.(0)
-  | Some op ->
+  | Break -> params.(0)
+  | op ->
      let val_a = params.(params.(pos + 1)) in
      let val_b = params.(params.(pos + 2)) in
      let target_pos = params.(pos + 3) in
      params.(target_pos) <- ((op_to_fun op) val_a val_b);
      execute ~params:params ~pos:(pos + 4)
-  | None -> raise Division_by_zero
 
-(* let print_array arr =
- *   String.concat ~sep:"; " (Array.to_list (Array.map ~f:string_of_int arr))
- *   |> print_string;
- *   print_newline () *)
+let adjust_input ~noun ~verb ~input =
+  input.(1) <- noun;
+  input.(2) <- verb
 
-let adjust_input =
-  input.(1) <- 12;
-  input.(2) <- 2
-
-let day02_1 =
-  adjust_input;
+let run ~noun ~verb =
+  let input =
+    Aoc_lib.csv_of_file "input02.txt"
+    |> List.map ~f:int_of_string
+    |> List.to_array
+  in
+  adjust_input ~noun:noun ~verb:verb ~input:input;
   execute ~params:input ~pos:0
 
-let () =
-  Printf.printf "Part 1: %d\n" (day02_1)
+let day02_1 = run ~noun:12 ~verb:2
 
+let day02_2 =
+  let rec iterative_run n v =
+    let res = run ~noun:n ~verb:v in
+    match res with
+    | 19690720 -> (n, v)
+    | _ -> match n with
+           | 99 -> iterative_run 0 (v + 1)
+           | _ -> iterative_run (n + 1) v
+  in
+  let (res_noun, res_verb) = iterative_run 0 0 in
+  100 * res_noun + res_verb
+
+let () =
+  Printf.printf "Part 1: %d\n" (day02_1);
+  Printf.printf "Part 2: %d\n" (day02_2)
