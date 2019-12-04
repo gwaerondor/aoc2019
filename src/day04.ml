8a98228d01d8e71db67d3a6f7725924b5cc8b4dc
Robin Larsson <gwaerondor@gmail.com>
2019-12-04 16:01:08 +0800

    Add solution to day04 part 2.

--- src/day04.ml
+++ src/day04.ml
@@ -11,16 +11,32 @@ let rec has_double = function
   | a::b::_ when a = b -> true
   | _::b::r -> has_double (b::r)
 
-let rec password_rule_matches lower upper current =
+let rec has_strictly_double = function
+  | [] -> false
+  | a::r -> let pred = (=) a in
+            if List.take_while ~f:pred r |> List.length |> (=) 1
+            then true
+            else has_strictly_double (List.drop_while ~f:pred r)
+
+let rec password_rule_matches lower upper current rule =
   if current > upper
   then []
   else
     let pw = Aoc_lib.char_list_of_int current in
-    if (is_monotonically_increasing pw) && (has_double pw)
-    then current :: (password_rule_matches lower upper (current + 1))
-    else (password_rule_matches lower upper (current + 1))
+    if rule pw
+    then current :: (password_rule_matches lower upper (current + 1) rule)
+    else (password_rule_matches lower upper (current + 1) rule)
+
+let lax_rule pw =
+  (is_monotonically_increasing pw) && (has_double pw)
+
+let strict_rule pw =
+  (is_monotonically_increasing pw) && (has_strictly_double pw)
 
 let day04_1 =
-  password_rule_matches 245182 790572 245182
+  password_rule_matches 245182 790572 245182 lax_rule
   |> List.length
-  |> Printf.printf "Part 1: %d\n"
+  |> Printf.printf "Part 1: %d\n";
+  password_rule_matches 245182 790572 245182 strict_rule
+  |> List.length
+  |> Printf.printf "Part 2: %d\n"
