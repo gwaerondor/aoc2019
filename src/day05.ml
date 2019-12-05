9043bd4cf1729790c70f1f861d8044f17f298c9e
Robin Larsson <gwaerondor@gmail.com>
2019-12-05 15:27:55 +0800

    Prepare to extend intcode with modes.

---
+++ src/day05.ml
@@ -0,0 +1,8 @@
+open Core
+open Aoc_lib
+open Intcode
+
+let day05_1 = -1
+
+let () =
+  Printf.printf "Part 1: %d\n" day05_1
--- src/intcode.ml
+++ src/intcode.ml
@@ -1,26 +1,41 @@
 open Core
 open Aoc_lib
-type op = Add | Mul | Break
+type op = Add | Mul | Break | Input | Output
+type parameter_mode = Position | Immediate
 exception Bad_argument
 
+let pad_to_two = function
+  | [c] -> ['0'; c]
+  | cs -> cs
+
+let parse_operator data =
+  char_list_of_int data |> pad_to_two |> String.of_char_list
+
 let get_operator params pos =
-  match params.(pos) with
-  | 1 -> Add
-  | 2 -> Mul
-  | 99 -> Break
+  match params.(pos) |> parse_operator with
+  | "01" -> (Position, Add)
+  | "02" -> (Position, Mul)
+  | "99" -> (Position, Break)
   | _ -> raise Bad_argument
 
 let op_to_fun = function
   | Add -> ( + )
   | Mul -> ( * )
-  | Break -> raise Bad_argument
+  | _ -> raise Bad_argument
+
+let get_value state mode arg =
+  match mode with
+  | Position -> state.(state.(arg))
+  | Immediate -> arg
 
 let rec execute ~state ~pos =
   match get_operator state pos with
-  | Break -> state.(0)
-  | op ->
-     let val_a = state.(state.(pos + 1)) in
-     let val_b = state.(state.(pos + 2)) in
+  | (_, Break) -> state.(0)
+  | (_, Input) -> raise Bad_argument
+  | (_, Output) -> raise Bad_argument
+  | (mode, op) ->
+     let val_a = get_value state mode (pos + 1) in
+     let val_b = get_value state mode (pos + 2) in
      let target_pos = state.(pos + 3) in
      state.(target_pos) <- ((op_to_fun op) val_a val_b);
      execute ~state:state ~pos:(pos + 4)
